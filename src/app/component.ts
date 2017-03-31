fc1a52882330dce3c26d3ef96da8e7084d6dce66
gayanper <gayanper@gmail.com>
2017-03-31 21:01:20 +0530

    enhance the stater code
    removed typings
    add support for node fs module

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,6 +1,18 @@
 import { Component } from '@angular/core';
+import * as fs from 'fs';
+
 @Component({
   selector: 'ha-app',
-  template: '<h1>Hello Angular2</h1>'
+  template: '<h1>Hello Angular2 {{message}}</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+  private message:string;
+
+  constructor() {
+    if(fs.existsSync("./db.json")) {
+      this.message = "exist";
+    } else {
+      this.message = "not exist";
+    }
+  }
+}
